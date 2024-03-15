eb1b4670b5ab527bddc93dd3d2838f026bce8cc5
Mahran Elmasri <mahran_masri@yahoo.com>
2024-03-15 09:21:01 +0100

    first commit

---
+++ src/api/doctor-room/controllers/doctor-room.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * doctor-room controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::doctor-room.doctor-room');
---
+++ src/api/doctor-room/routes/doctor-room.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * doctor-room router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::doctor-room.doctor-room');
